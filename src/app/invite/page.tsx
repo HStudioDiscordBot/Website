277cbeaa51be04c875bece576d7a66e89d8e8cd1
HewkawAr <hewkawar@gmail.com>
2024-06-09 21:15:10 +0700

    Update page.tsx

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -17,7 +17,7 @@ export default function Invite() {
                     <img src="/profile/1.png" alt="HStudio 1" className="w-[300px] rounded-full" />
                     <div className="flex flex-col justify-around ml-5 text-center">
                         <p className="text-2xl">HStudio (1)</p>
-                        <Link href="https://discord.com/oauth2/authorize?client_id=1105873690022924450" target="_blank" className="text-[#5c9ae0] w-[160px] bg-[#3d4c61] p-3 rounded hover:bg-[#475870] transition-colors duration-150">Invite HStudio (1)</Link>
+                        <Link href="https://discord.com/oauth2/authorize?client_id=1140130682094497823" target="_blank" className="text-[#5c9ae0] w-[160px] bg-[#3d4c61] p-3 rounded hover:bg-[#475870] transition-colors duration-150">Invite HStudio (1)</Link>
                     </div>
                 </div>
             </div>
