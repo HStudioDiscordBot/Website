685203f060672dcc0518255692f7bdf145b480de
HewkawAr <hewkawar@gmail.com>
2024-06-09 22:16:58 +0700

    Update page.tsx

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,24 +5,23 @@ import Link from "next/link";
 export default function Invite() {
     return (
         <>
+            <h1 className="text-2xl lg:text-4xl">บอทในเครือ HStudio</h1>
             <div className="flex flex-raw flex-wrap justify-center">
                 <div className="bg-[#414141] p-5 flex rounded m-5">
-                    <img src="/profile/0.png" alt="HStudio Main" className="w-[300px] rounded-full" />
+                    <img src="/profile/0.png" alt="HStudio Main" className="w-[100px] rounded-full" />
                     <div className="flex flex-col justify-around ml-5 text-center">
                         <p className="text-2xl">HStudio</p>
-                        <Link href="https://discord.com/oauth2/authorize?client_id=1105873690022924450" target="_blank" className="text-[#5c9ae0] w-[160px] bg-[#3d4c61] p-3 rounded hover:bg-[#475870] transition-colors duration-150">Invite HStudio</Link>
+                        <Link href="https://discord.com/oauth2/authorize?client_id=1105873690022924450" target="_blank" className="text-[#5c9ae0] w-[120px] bg-[#3d4c61] py-1 px-2 rounded hover:bg-[#475870] transition-colors duration-150">Invite HStudio</Link>
                     </div>
                 </div>
                 <div className="bg-[#414141] p-5 flex rounded m-5">
-                    <img src="/profile/1.png" alt="HStudio 1" className="w-[300px] rounded-full" />
+                    <img src="/profile/1.png" alt="HStudio 1" className="w-[100px] rounded-full" />
                     <div className="flex flex-col justify-around ml-5 text-center">
                         <p className="text-2xl">HStudio (1)</p>
-                        <Link href="https://discord.com/oauth2/authorize?client_id=1140130682094497823" target="_blank" className="text-[#5c9ae0] w-[160px] bg-[#3d4c61] p-3 rounded hover:bg-[#475870] transition-colors duration-150">Invite HStudio (1)</Link>
+                        <Link href="https://discord.com/oauth2/authorize?client_id=1140130682094497823" target="_blank" className="text-[#5c9ae0] w-[120px] bg-[#3d4c61] py-1 px-2 rounded hover:bg-[#475870] transition-colors duration-150">Invite HStudio (1)</Link>
                     </div>
                 </div>
             </div>
-
-
         </>
     )
 }
