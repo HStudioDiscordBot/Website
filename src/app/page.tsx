535d9d9b7fd1b2c4804c222f3573e38f2561f00b
HewkawAr <hewkawar@gmail.com>
2024-06-09 22:10:22 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,10 @@ export default function Home() {
 
   return (
     <>
-      <div className="bg-[#414141] w-[50vw] rounded flex flex-col items-center p-5 mt-10">
+      <div className="bg-[#414141] w-[100vw] md:w-[80vw] lg:w-[60vw] xl:w-[40vw] rounded flex flex-col items-center p-5 mt-10 text-wrap text-center">
         <img src="/HStudio.webp" width={300} height={300} alt="HStudio Logo" />
         <h1 className="text-4xl mt-3">HStudio (เวอร์ชั่น {version})</h1>
-        <div className="p-5">
+        <div className="p-5 flex flex-wrap justify-center">
           <Link href="/invite" className="p-3 mx-2 text-[#5c9ae0] hover:bg-[#3d4c61] rounded transition-colors duration-150">เชิญบอท</Link>
           <Link href="https://discord.gg/yYydJ2tpcZ" target="_blank" className="p-3 mx-2 text-[#5c9ae0] hover:bg-[#3d4c61] rounded transition-colors duration-150">ดิสช่วยเหลือ</Link>
           <Link href="https://docs.hstudio.hewkawar.xyz/" target="_blank" className="p-3 mx-2 text-[#5c9ae0] hover:bg-[#3d4c61] rounded transition-colors duration-150">คู่มือการใช้งาน</Link>
