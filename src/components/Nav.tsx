593ec51b92bb0ad16c8eb6659fd299ce2de43144
HewkawAr <hewkawar@gmail.com>
2024-06-09 22:08:48 +0700

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function Nav() {
     return (
         <nav className="p-5 flex justify-between">
-            <div>
-                <ul className="text-white flex">
+            <div className="text-center">
+                <ul className="text-white flex flex-wrap justify-center">
                     <li><Link className="p-3 hover:text-[#5c9ae0] transition-colors duration-150" href="/">หน้าหลัก</Link></li>
                     <li><Link className="p-3 hover:text-[#5c9ae0] transition-colors duration-150" href="/status">สถานะบอท</Link></li>
                     <li><Link className="p-3 hover:text-[#5c9ae0] transition-colors duration-150" href="/terms-of-service">ข้อกำหนดในการให้บริการ</Link></li>
