af6285619e4c511e4637d9de51ed1554becc4146
HewkawAr <hewkawar@gmail.com>
2024-06-13 21:40:49 +0700

    SCO Optimize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Prompt } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
@@ -7,9 +7,22 @@ const inter = Prompt({ subsets: ["thai"], weight: "500" });
 
 export const metadata: Metadata = {
   title: "HStudio",
-  description: "บอทเพลงที่สร้างมาสำหรับทุกคน ! | บอท, บอทเพลง, บอทเพลงดิส, บอทเพลงดิสคอร์ด, บอทเพลง Discord, Discord, Discord Bot, discord bot, discord music bot, bot music, bot music discord, บอทเปิดเพลงในดิส, บอทเพลง ดิส, บอทเพลง ดิสคอร์ด, บอทเพลง Dis, บอทเปิดเพลงในไทย, บอทเพลงไทย, บอทเพลงดิสไทย",
-  keywords: "บอท, บอทเพลง, บอทเพลงดิส, บอทเพลงดิสคอร์ด, บอทเพลง Discord, Discord, Discord Bot, discord bot, discord music bot, bot music, bot music discord, บอทเปิดเพลงในดิส, บอทเพลง ดิส, บอทเพลง ดิสคอร์ด, บอทเพลง Dis, บอทเปิดเพลงในไทย, บอทเพลงไทย, บอทเพลงดิสไทย",
+  description: "บอทเพลงที่สร้างมาเพื่อทุกคน!",
+  icons: ["/favicon.ico"],
+  keywords: ["บอท", "บอทเพลง", "บอทเพลงดิส", "บอทเพลงดิสคอร์ด", "บอทเพลง Discord", "Discord", "Discord Bot", "discord bot", "discord music bot", "bot music", "bot music discord", "บอทเปิดเพลงในดิส", "บอทเพลง ดิส", "บอทเพลง ดิสคอร์ด", "บอทเพลง Dis", "บอทเปิดเพลงในไทย", "บอทเพลงไทย", "บอทเพลงดิสไทย"],
+  publisher: "HewkawAr",
+  openGraph: {
+    title: "HStudio",
+    url: "https://hstudio.hewkawar.xyz/",
+    images: "https://hstudio.hewkawar.xyz/HStudio.profile.banner.png",
+    locale: "th",
+    type: "website"
+  }
 };
+
+export const viewport: Viewport = {
+  themeColor: "#5c9ae0"
+}
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
