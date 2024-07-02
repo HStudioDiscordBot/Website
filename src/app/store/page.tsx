7bf778a2c9071580a4eace2a3571e8f616dbc606
HewkawAr <hewkawar@gmail.com>
2024-07-02 21:26:20 +0700

    Update page.tsx

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -72,7 +72,7 @@ export default function Store() {
           input: "select",
           inputOptions: {
             promptpay: currentLanguage == "thai" ? "พร้อมเพย์" : "Promptpay",
-            // truemoneyVoucher: currentLanguage == "thai" ? "ลิ้งค์อั่งเปาทรูมันนี่" : "Truemoney Voucher"
+            truemoneyVoucher: currentLanguage == "thai" ? "ลิ้งค์อั่งเปาทรูมันนี่" : "Truemoney Voucher"
           },
           confirmButtonText: currentLanguage == "thai" ? "ยืนยัน" : "Confirm",
           cancelButtonText: currentLanguage == "thai" ? "ยกเลิก" : "Cancel",
